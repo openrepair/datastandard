bb83539b95ae7a17147268dfc620c3944ff132f0
Neil Mather <ngmather@gmail.com>
2024-05-30 14:36:43 +0100

    Update language and copyright year

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ from recommonmark.transform import AutoStructify
 # -- Project information -----------------------------------------------------
 
 project = u'Open Repair Data Standard'
-copyright = u'2021, Open Repair Alliance'
+copyright = u'2024, Open Repair Alliance'
 author = u'Open Repair Alliance'
 
 # The short X.Y version
@@ -69,7 +69,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
